a459f5db3df0a1b0d9aba99683edd9d9e674ed5b
rayd512 <rdizon@ualberta.ca>
2020-04-30 01:21:34 -0600

    Word displays correctly when clicking on icon. Cannot figure out why if statements won't work

--- content.js
+++ content.js
@@ -1,19 +1,23 @@
-// chrome.runtime.onMessage.addListener(gotMessage);
-
-// function gotMessage(request, sender, sendResponse) {
-//     console.log(message.txt);
-// }
-// console.log("In content")
 var divWord = document.getElementById("currentWord")
+// console.log(location.href)
+// if (location.href.includes("skribble.io"))
 if (divWord != null) {
     let word = divWord.textContent
     if (word.includes("_")) {
         // alert("NO")
-        chrome.runtime.sendMessage("Guessing", function(response){
-            console.log("Sending message")
+        message = "<strong>Not Your Turn</strong>"
+        chrome.runtime.sendMessage(message, function(response){
+            console.log(response)
+        });
+    } else {
+        word = "<strong>" + word + "</strong>"
+        chrome.runtime.sendMessage(word, function(response){
+            console.log(response)
         });
     }
 } else {
+    chrome.runtime.sendMessage("no_word", function(response){
+        console.log(response)
+    });
     console.log("Could not find the current word")
 }
-// alert("Content")
--- popup.js
+++ popup.js
@@ -4,16 +4,25 @@ chrome.tabs.query({active: true, currentWindow: true}, gotTabs)
 function gotTabs(tabs) {
         chrome.tabs.executeScript(tabs[0].id, {
             "file": "content.js"
-        }, function (response) { 
+        }, function () { 
             console.log("Script Executed .. ");
-            document.getElementById("Skribbl-word").innerHTML = response
+
         });
 }
 
-// function onReq(request, sender, sendResponse)
-// {
-//     console.log("msg: "+request);
-//     document.getElementById("Skribbl-word").innerHTML = request;
-// }
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    changeWord = document.getElementById("Skribbl-word").innerHTML
+    console.log("Message recieved: " + message)
+    // if (message == "guessing") {
+    //     changeWord = "Not your turn"
+    // } else if (message == "no_word") {
+    //     changeWord = "No word found"
+    // } else {
+    //     changeWord = word
+    // }
+    document.getElementById("Skribbl-word").innerHTML = message
 
-// chrome.runtime.onMessage.addListener(onReq);
+    sendResponse({
+        data: "I am fine, thank you. How is life in the background?"
+    });
+});
