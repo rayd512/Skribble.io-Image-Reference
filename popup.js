9c0af8ab8adba5a6eda1835efe2b0c076cb24a01
rayd512 <rdizon@ualberta.ca>
2020-05-01 00:08:12 -0600

    Integrated API, one image of word loads

--- popup.js
+++ popup.js
@@ -21,8 +21,25 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     //     changeWord = word
     // }
     document.getElementById("Skribbl-word").innerHTML = message
-
+    console.log("Getting Images")
+    getImages(message)
+    console.log("Set Image")
     sendResponse({
-        data: "I am fine, thank you. How is life in the background?"
+        data: "Got message"
     });
 });
+
+function getImages(message) {
+    let UNSPLASH_API_BASE = "https://api.unsplash.com/search/photos?client_id=HLccHhMKXhvNVrUpI97pzEpg_jABsIxp_MWGVXv6hmU&query="
+    var request = new XMLHttpRequest()
+    var REQUEST_URL = UNSPLASH_API_BASE + message
+    request.open('GET', REQUEST_URL, true)
+    console.log("Opened Request")
+    // console.log(JSON.parse(this.response))
+    request.onload = function() {
+        var data = JSON.parse(this.response)
+        document.getElementById("reference").src = data.results[0].urls.regular;
+        console.log("Element src set")
+    }
+    request.send()
+}
